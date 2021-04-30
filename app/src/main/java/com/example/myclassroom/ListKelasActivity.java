32eaaa1bf48ad6d3f98e2a91eefd7835e3b4e4a4
exceldeo <52096462+exceldeo@users.noreply.github.com>
2021-04-30 11:51:35 +0700

    menambahkan cardview dan page list class

---
+++ app/src/main/java/com/example/myclassroom/ListKelasActivity.java
@@ -0,0 +1,14 @@
+package com.example.myclassroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListKelasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_kelas);
+    }
+}
