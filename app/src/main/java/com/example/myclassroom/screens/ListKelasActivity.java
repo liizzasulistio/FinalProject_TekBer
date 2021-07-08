c059c5f789605beed418672fb5765e207acba22f
exceldeo <52096462+exceldeo@users.noreply.github.com>
2021-07-08 22:31:35 +0700

    menambahkan menu

--- app/src/main/java/com/example/myclassroom/screens/ListKelasActivity.java
+++ app/src/main/java/com/example/myclassroom/screens/ListKelasActivity.java
@@ -1,14 +1,19 @@
 package com.example.myclassroom.screens;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.Toolbar;
 
 import com.example.myclassroom.R;
 import com.example.myclassroom.adapter.AdapterKelas;
@@ -18,24 +23,24 @@ import com.google.android.material.navigation.NavigationView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListKelasActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+public class ListKelasActivity extends AppCompatActivity  {
 
     RecyclerView KelasRecyclerView;
     AdapterKelas kelasAdapter;
     List<DummyData.DataKelas> mData;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_kelas);
-//        setSupportActionBar();
+
         setupItemWiew();
+
         loadData();
         setupView();
     }
 
     private void setupItemWiew() {
-
         KelasRecyclerView = findViewById(R.id.rv_kelas);
         mData = new ArrayList<>();
     }
@@ -56,13 +61,37 @@ public class ListKelasActivity extends AppCompatActivity implements NavigationVi
         KelasRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
+
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 
     @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if(id == R.id.nav_list_kelas){
+            Intent intent = new Intent(this, ListKelasActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        if (id == R.id.nav_tambah_kelas) {
+            Intent intent = new Intent(this, TambahKelasActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        if (id == R.id.nav_profile) {
+        }
+        if (id == R.id.nav_logout) {
+        }
+
+        return super.onOptionsItemSelected(item);
     }
+
 }
