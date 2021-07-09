ba1ba8a5eb549d07eca22b483985e6dd157fe243
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-07-09 19:19:36 +0700

    add edit student

--- app/src/main/java/com/example/myclassroom/screens/EditStudentActivity.java
+++ app/src/main/java/com/example/myclassroom/screens/EditStudentActivity.java
@@ -1,6 +1,10 @@
 package com.example.myclassroom.screens;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,12 +12,36 @@ import com.example.myclassroom.R;
 
 public class EditStudentActivity extends AppCompatActivity
 {
+    TextView studentName, studentNRP;
+    EditText studentScore;
+    private String value;
+    private Button btnSave;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_student);
+
+        studentName = (TextView) findViewById(R.id.studentName);
+        studentNRP = (TextView) findViewById(R.id.studentNRP);
+        studentScore = (EditText) findViewById(R.id.edtNilai);
+        btnSave = (Button) findViewById(R.id.btnSave);
+
+
+        if (getIntent().getStringExtra("student") != null) {
+            value = getIntent().getStringExtra("student");
+            studentName.setText(value);
+            String val2 = getIntent().getStringExtra("student_nrp");
+            studentNRP.setText(val2);
+        }
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/myclassroom/screens/StudentDetail.java
+++ app/src/main/java/com/example/myclassroom/screens/StudentDetail.java
@@ -3,6 +3,7 @@ package com.example.myclassroom.screens;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,6 +16,7 @@ public class StudentDetail extends AppCompatActivity {
 
     private TextView student, nrp, nilai_angka, nilai_huruf;
     private Button btnEdit;
+    private String value, val2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,10 +30,21 @@ public class StudentDetail extends AppCompatActivity {
 //        Bundle bundle = intent.getExtras();
 
         if (getIntent().getStringExtra("student") != null) {
-            String value = getIntent().getStringExtra("student");
+            value = getIntent().getStringExtra("student");
             student.setText(value);
-            String val2 = getIntent().getStringExtra("student_nrp");
+            val2 = getIntent().getStringExtra("student_nrp");
             nrp.setText(val2);
         }
+
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StudentDetail.this, EditStudentActivity.class);
+                intent.putExtra("student", value);
+                intent.putExtra("student_nrp", val2);
+                startActivity(intent);
+            }
+        });
     }
 }
